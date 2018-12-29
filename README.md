5a24d2ac065e9c65e750a1a8d5f4ed894ce9604a
chris-schmitz <schmitz.chris@gmail.com>
2018-12-29 16:14:05 -0600

    Adds pokemon code and sounds. fills out readme more

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hello-rfid
-A simple hello world I used to start learning RFID
+
+A simple hello world I used to start learning RFID.
+
+![demo](readme_attachments/demo.gif)
+
+This is yet another hello-world that I put a bit more effort into than I should have :P
+
+I wanted a way of using RFID to trigger some distinct actions, so the plan was:
+
+-   Make some pokemon figurines with RFID tags embedded into them.
+-   Play lights and sounds when the RFID reader detected a specific pokemon.
+
+## Links
+
+-   [Video showing off the hello-world](https://vimeo.com/308720192)
+-   [Thingiverse models](https://www.thingiverse.com/thing:3321541)
--- hello-rfid.ino
+++ hello-rfid.ino
@@ -1,67 +1,122 @@
-/*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- *
- * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- *
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- *
- * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
- * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
- * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
- * keep the PICCs at reading distance until complete.
- *
- * @license Released into the public domain.
- *
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- */
-
+// * Pull in dependencies
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Adafruit_NeoPixel.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
 
-#define RST_PIN 16 // Configurable, see typical pin layout above
-#define SS_PIN 25  // Configurable, see typical pin layout above
+// * Define constants for the music maker wing
+#define VS1053_RESET -1
+#define VS1053_CS 6
+#define VS1053_DCS 10
+#define VS1053_DREQ 9
+#define CARDCS 5
+#define DEFAULT_VOLUME 3
+#define USING_MUSIC_MAKER_WING true
 
-#define NEOPIXEL 15
+// * Define constants for the RFID reader
+#define SS_PIN 19  // Configurable, see typical pin layout above
+#define RST_PIN 11 // Configurable, see typical pin layout above
+// ! Note that I'm using the Feather M0 Express for this project and to get the
+// ! card reader to work I had to use something other than the SDA pin. I have no idea why,
+// ! but I did. While troubleshooting I found this answer on stack exchange that didn't directly
+// ! apply to my issue, but it did suggest that you could use any pin for the select and not just
+// ! the SDA pin:
+// ? https://arduino.stackexchange.com/a/58030/35534
+
+// * Define constants for neopixel bar
+#define NEOPIXEL 12
 #define TOTAL_PIXELS 8
 
+// * Instantiate our classes
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 Adafruit_NeoPixel bar = Adafruit_NeoPixel(TOTAL_PIXELS, NEOPIXEL, NEO_GRB + NEO_KHZ800);
+Adafruit_VS1053_FilePlayer player = Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
 
+// * setup variables for the RFID tags we're looking for
 String content = "";
 String targetTag1 = "04 72 d2 4a e6 4c 81";
 String targetTag2 = "04 52 c5 4a e6 4c 80";
 
+// * setup pokemon RFID tags
+String bulbasaur = "04 60 d0 4a e6 4c 81";
+String charmander = "04 78 d2 4a e6 4c 81";
+String squirtle = "";
+String pikachu = "";
+
 void setup()
 {
 	Serial.begin(9600); // Initialize serial communications with the PC
-	while (!Serial)
-		;							   // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	// while (!Serial)
+	// 	; // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+	setupRFIDCardReader();
+	setupNeopixelBar();
+	setupMusicMakerWing();
+	signalReady();
+
+	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
+
+void setupMusicMakerWing()
+{
+	if (!USING_MUSIC_MAKER_WING)
+	{
+		return;
+	}
+
+	if (!player.begin())
+	{
+		Serial.println(F("Could not find the music maker wing. Either add the wing or turn the wing usage off in the code."));
+		while (1)
+			;
+	}
+
+	Serial.println(F("Music Maker wing found"));
+
+	if (!SD.begin(CARDCS))
+	{
+		Serial.println(F("SD card either failed or not found."));
+		while (1)
+			;
+	}
+	Serial.println("SD Card Ok");
+	// printDirectory(SD.open("/"), 0);
+
+	player.setVolume(DEFAULT_VOLUME, DEFAULT_VOLUME);
+	player.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+
+	// player.sineTest(0x44, 500);
+}
+
+void setupRFIDCardReader()
+{
 	SPI.begin();					   // Init SPI bus
 	mfrc522.PCD_Init();				   // Init MFRC522
 	mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+									   // Serial.println(mfrc522.PCD_GetAntennaGain());
+									   // mfrc522.PCD_SetAntennaGain(mfrc522.PCD_RxGain.RxGain_48dB);
+									   // Serial.println(mfrc522.PCD_GetAntennaGain());
+}
 
+void setupNeopixelBar()
+{
 	bar.begin();
+}
 
-	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+void signalReady()
+{
+	for (int i = 0; i <= TOTAL_PIXELS; i++)
+	{
+		bar.setPixelColor(i, 89, 209, 14);
+		bar.show();
+		delay(90);
+	}
+	for (int i = TOTAL_PIXELS; i >= 0; i--)
+	{
+		bar.setPixelColor(i, 0, 0, 0);
+		bar.show();
+		delay(90);
+	}
 }
 
 bool readACardOnThePreviousLoop = false;
@@ -107,10 +162,31 @@ void loop()
 		readACardOnThePreviousLoop = true;
 		fillBar(0, 255, 255);
 	}
+	else if (content.substring(1) == charmander)
+	{
+
+		fillBar(255, 0, 0);
+		readACardOnThePreviousLoop = true;
+		player.playFullFile("CHARMA~1.MP3");
+	}
+	else if (content.substring(1) == bulbasaur)
+	{
+
+		fillBar(0, 255, 0);
+		readACardOnThePreviousLoop = true;
+		player.playFullFile("BULBAS~1.MP3");
+	}
+	else if (content.substring(1) == squirtle)
+	{
+
+		fillBar(0, 0, 255);
+		readACardOnThePreviousLoop = true;
+		player.playFullFile("SQUIRTLE.MP3");
+	}
 	else
 	{
-		readACardOnThePreviousLoop = false;
 		Serial.println("no tag match found");
+		readACardOnThePreviousLoop = false;
 	}
 }
 
@@ -145,4 +221,35 @@ void captureUID()
 		content.concat(String(mfrc522.uid.uidByte[i], HEX));
 	}
 	// Serial.println();
+}
+void printDirectory(File dir, int numTabs)
+{
+	while (true)
+	{
+
+		File entry = dir.openNextFile();
+		if (!entry)
+		{
+			// no more files
+			//Serial.println("**nomorefiles**");
+			break;
+		}
+		for (uint8_t i = 0; i < numTabs; i++)
+		{
+			Serial.print('\t');
+		}
+		Serial.print(entry.name());
+		if (entry.isDirectory())
+		{
+			Serial.println("/");
+			printDirectory(entry, numTabs + 1);
+		}
+		else
+		{
+			// files have sizes, directories do not
+			Serial.print("\t\t");
+			Serial.println(entry.size(), DEC);
+		}
+		entry.close();
+	}
 }
